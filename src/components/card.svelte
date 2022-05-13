7a3ce76258e44c0132e47b8c9f4e335232039959
Viggo Petersen <vig56@hotmail.com>
2022-05-13 18:39:49 +0200

    Add front page with movies
    fixes #1

---
+++ src/components/card.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let image;
+  export let title;
+  export let id
+</script>
+
+<a class="card shadow" href="/movie/{id}">
+  <img src="https://image.tmdb.org/t/p/w300/{image}" class="card-img-top" alt="{title}" width="100%">
+  <div class="card-body">
+    <span class="card-title">{title}</span>
+  </div>
+</a>
+
+<style>
+  .card {
+    text-decoration: none;
+    color: black;
+    height: 100%;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,27 @@
 <script>
-  import Banner from '../components/banner.svelte';
+  import Card from '../components/card.svelte';
+  import { getPopularMovies } from '../lib/movieApi';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Banner></Banner>
+<div class="container">
+  <div class="row">
+    <div class="col-6 mx-auto">
+      <div class="input-group my-3">
+        <input type="text" class="form-control" placeholder="Search for movies..." />
+        <button class="btn btn-success" type="button" id="button-addon2">Search!</button>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    {#await getPopularMovies() then movies}
+      {#each movies as movie}
+        <div class="col-2 mb-3">
+          <Card image={movie.poster_path} title={movie.original_title} id={movie.id}></Card>
+        </div>
+      {/each}
+    {/await}
+  </div>
+</div>
 
 <style>
   h1 {
