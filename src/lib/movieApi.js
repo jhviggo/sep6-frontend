3f28ebe4ef4101e98322534521ac505630282059
Viggo Petersen <vig56@hotmail.com>
2022-05-16 19:45:31 +0200

    Add search functionality
    fixes #3
    
    Add simple search to the front page

--- src/lib/movieApi.js
+++ src/lib/movieApi.js
@@ -1,18 +1,24 @@
 import axios from 'axios';
 
 const BASE_URL = "https://api.themoviedb.org/3"
-const API_KEY = "0a4f417a73d715128e70e0b7ea481b72" // public api key isn't really secret
+const API_KEY = "0a4f417a73d715128e70e0b7ea481b72"
 
 const instance = axios.create({
   baseURL: BASE_URL,
 })
 
 async function getPopularMovies() {
-  const data = await instance.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await instance.get(`/movie/popular?api_key=${API_KEY}`);
+  return data.data.results;
+}
+
+async function searchMovies(term) {
+  const data = await instance.get(`/search/movie?api_key=${API_KEY}&query=${term}`);
   console.log(data.data.results);
   return data.data.results;
 }
 
 export {
-  getPopularMovies
+  getPopularMovies,
+  searchMovies
 };
