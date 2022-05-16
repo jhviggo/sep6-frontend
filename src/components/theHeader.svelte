1b233f2029278ea7e23161743a168b46c30416fa
Viggo Petersen <vig56@hotmail.com>
2022-05-16 20:15:25 +0200

    Add header in default layout
    solves #2

---
+++ src/components/theHeader.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<ul class="nav justify-content-center p-2">
+  <li class="nav-item mx-1">
+    <a class="nav-link" aria-current="page" href="/">Home</a>
+  </li>
+  <li class="nav-item mx-1">
+    <a class="nav-link" aria-current="page" href="/profile">Profile</a>
+  </li>
+  <li class="nav-item mx-1">
+    <a class="nav-link" aria-current="page" href="/top-list">Top list</a>
+  </li>
+</ul>
+
+<style>
+  .nav {
+    background-color: orangered;
+  }
+
+  a {
+    color: white;
+  }
+
+  a:hover {
+    color: black;
+  }
+</style>
