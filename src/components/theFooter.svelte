bf5ac80670005a2e19370266baccb1956884fd5c
Viggo Petersen <vig56@hotmail.com>
2022-05-20 13:57:42 +0200

    Add footer
    fixes #6
    
    Adds a footer with study and developers information

---
+++ src/components/theFooter.svelte
@@ -0,0 +1,53 @@
+<script>
+
+</script>
+
+<footer class="mt-5">
+  <div class="container">
+    <div class="row py-5">
+      <div class="col-auto area">
+        <p>Study</p>
+        <img class="via-logo" src="/img/via.png" alt="VIA University College"/>
+      </div>
+      <div class="col area">
+        <p>About us</p>
+        <a href="https://github.com/jannikschwarz">
+          <img class="github-logo" src="/img/github.png" alt="Github"/>
+          Jannik Schwarz
+        </a>
+        <a href="https://github.com/jhviggo">
+          <img class="github-logo" src="/img/github.png" alt="Github"/>
+          Viggo Petersen
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<style>
+  footer {
+    border-top: 1px solid lightgray;
+  }
+
+  .area {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .via-logo {
+    height: 5rem;
+  }
+
+  .area a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .area p {
+    font-weight: bold;
+  }
+
+  .github-logo {
+    height: 1rem;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
   import TheHeader from "../components/theHeader.svelte";
+  import TheFooter from "../components/theFooter.svelte";
 </script>
 
 <TheHeader />
 <slot></slot>
+<TheFooter />
 
 <style global>
   :global(body) {
